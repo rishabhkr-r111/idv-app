23f1ffcdf6ad1b37d38ed1b0a043ac9586514d8c
Varun Panyam <2003varunp@gmail.com>
2023-08-29 21:24:49 +0530

    added frontend

--- frontend/src/comp/ImgUpload.jsx
+++ frontend/src/comp/ImgUpload.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 const API_URL = import.meta.env.VITE_API_URL;
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { AiOutlineCamera } from "react-icons/ai";
 
 function ImgUpload() {
   const navigate = useNavigate();
@@ -9,6 +11,7 @@ function ImgUpload() {
 
   const handleImageChange = (event) => {
     setSelectedImage(event.target.files[0]);
+    // handleUpload();
   };
 
   const handleUpload = async () => {
@@ -46,9 +49,13 @@ function ImgUpload() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <div className="max-w-md p-8 bg-white rounded-lg shadow-md">
-        <h2 className="text-xl font-semibold mb-4 text-center">Image Upload</h2>
+    <div className="w-screen h-screen flex flex-col items-center justify-center bg-gradient-to-bl from-gray-700 via-gray-900 to-black">
+      <div className="p-8 bg-white rounded-xl flex flex-col h-2/3 w-1/2 items-center">
+        <span className="text-2xl font-bold">Upload an Image</span>
+
+        {/* <h2 className="text-xl font-black mb-4 text-center tracking-tighter">
+          Image Upload
+        </h2>
         <div className="flex flex-col items-center">
           <input
             type="file"
@@ -65,8 +72,56 @@ function ImgUpload() {
         </div>
         {response && (
           <p className="mt-4 text-center">Server Response: {response}</p>
-        )}
+        )} */}
+
+        <div
+          className="flex items-center justify-center w-full mt-20"
+          onClick={handleUpload}
+          onChange={handleImageChange}
+        >
+          <label
+            htmlFor="dropzone-file"
+            className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer hover:bg-gray-100"
+          >
+            <div className="flex flex-col items-center justify-center pt-5 pb-6">
+              <svg
+                className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 16"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                />
+              </svg>
+              <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                <span className="font-semibold">Click to upload</span> or drag
+                and drop
+              </p>
+              <p className="text-xs text-gray-500 dark:text-gray-400">
+                SVG, PNG, JPG or GIF (MAX. 800x400px)
+              </p>
+            </div>
+            <input id="dropzone-file" type="file" className="hidden" />
+          </label>
+        </div>
       </div>
+      <div className="flex gap-4 items-center mt-7 bg-white rounded-lg px-7 justify-between py-3">
+        <button>
+          <AiOutlineCloudUpload size={30} />
+        </button>
+        <button>
+          <AiOutlineCamera size={30} />
+        </button>
+      </div>
+      {response && (
+        <p className="mt-4 text-center">Server Response: {response}</p>
+      )}
     </div>
   );
 }
